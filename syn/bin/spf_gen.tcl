0caf35225fb29ce371871950c04aa9f63e204153
emadddm98 <157698921+emadddm98@users.noreply.github.com>
2024-01-29 00:23:06 +0100

    Add files via upload

---
+++ syn/bin/spf_gen.tcl
@@ -0,0 +1,66 @@
+set GATE_PATH ../out
+set LOG_PATH  ../log
+set TECH 	  NangateOpenCellLibrary
+set TOPLEVEL  cv32e40p_top
+
+# ------------------------------------------------------------------------------- #
+# Libraries, Design Sources                                                       #
+# ------------------------------------------------------------------------------- #
+
+set search_path [ join "../techlib/ $search_path" ]
+set search_path [ join "$GATE_PATH $search_path" ]
+
+if { $TECH == "NangateOpenCellLibrary" } {
+   uplevel #0 source ../bin/NangateOpenCell.dc_setup_scan.tcl
+} else {
+   puts "Post synthesis scan-insertion is supported ONLY for Nangate"
+   quit
+}
+
+read_verilog ${GATE_PATH}/cv32e40p_top.v
+
+# ------------------------------------------------------------------------------- #
+#  Elaborate, Link & Check                                                        #
+# ------------------------------------------------------------------------------- #
+
+current_design $TOPLEVEL
+link
+check_design
+report_area
+
+# ------------------------------------------------------------------------------- #
+#  Scan Chain Insertion & Test Protocol Generation                                #
+# ------------------------------------------------------------------------------- #
+
+### Useful pins functionality options #############################################
+# set_dft_signal -view existing_dft -type ScanEnable -port test_en_i 
+# -active_state 1
+# set_dft_signal -view existing_dft -type ScanClock -timing { 1 20 } -port clk_i
+# set_dft_signal -view existing_dft -type Reset -active_state 0 -port rst_ni
+###################################################################################
+
+set_dft_signal -view existing_dft -type Constant -port pulp_clock_en_i -active_state 1
+
+create_test_protocol -infer_asynch -infer_clock
+dft_drc
+preview_dft
+insert_dft
+
+streaming_dft_planner
+
+# ------------------------------------------------------------------------------- #
+#  Export                                                                         #
+# ------------------------------------------------------------------------------- #
+
+change_names -rules verilog -hierarchy
+#report_scan_path -test_mode all
+
+report_area
+
+#write -hierarchy -format verilog -output "${GATE_PATH}/cv32e40p_top_scan.v"
+#write_sdf -version 3.0 "${GATE_PATH}/cv32e40p_top_scan.sdf"
+#write_sdc "${GATE_PATH}/cv32e40p_top_scan.sdc"
+write_test_protocol -output cv32e40p_top_scan.spf -test_mode Internal_scan
+write_test_protocol -output cv32e40p_top_scancompress.spf -test_mode ScanCompression_mode
+
+quit
